fbdc830ac63ec6def301811d3e2b8786c9b65fe4
mehdishokrani <mehdishokrani1@gmail.com>
2023-08-19 16:25:02 -0600

    add dotenv module and .env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -29,8 +30,7 @@ const userSchema = new Schema(
     }
     );
 
-var secret = "This is my Secret Key"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields:["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:["password"] });
 
 const User = mongoose.model("User",userSchema);
 
